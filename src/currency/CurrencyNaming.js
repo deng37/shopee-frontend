8691f3a1d6269c52a15f988ab8fdee9f84186721
Denver Tendri Haryanto <Denver Tendri Haryanto>
2018-11-07 01:02:55 +0700

    Currency - fix wrong naming

--- src/currency/CurrencyNaming.js
+++ src/currency/CurrencyNaming.js
@@ -5,12 +5,15 @@ export const getCurrencyName = async (currencyCode) => {
   try {
      const response = await fetch(mainConfig.API_CURRENCY_NAMING_URL + currencyCode + "?fields=currencies");
      const responseJson = await response.json();
-     return trimNaming(responseJson);
+     return trimNaming(responseJson, currencyCode);
    } catch (error) {
       console.error(error);
    }
 }
 
-function trimNaming(result) {
+function trimNaming(result, currencyCode) {
+  for (var itr=0; itr<=result[0].currencies.length; itr++) {
+    if (result[0].currencies[itr].code === currencyCode) return  result[0].currencies[itr].name
+  }
   return result[0].currencies[0].name;
 }
